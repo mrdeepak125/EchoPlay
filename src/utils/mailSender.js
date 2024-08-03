45d523d1b7df03a6f1805d7f3afb5a2dfd27f082
mrdeepak125 <asus0gl8@outlook.com>
2024-08-03 10:08:58 +0530

    update

--- src/utils/mailSender.js
+++ src/utils/mailSender.js
@@ -14,7 +14,7 @@ const mailSender = async (email, title, body) => {
 
 
             let info = await transporter.sendMail({
-                from: `"Hayasaka" <${process.env.MAIL_USER}>`,
+                from: `"EchoPlay" <${process.env.MAIL_USER}>`,
                 to:`${email}`,
                 subject: `${title}`,
                 html: `${body}`,
