c1f9d6d89a6883cd3a8df4772c5a6593208bec06
mrdeepak125 <asus0gl8@outlook.com>
2024-10-19 09:49:10 +0530

    fix error

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -7,7 +7,7 @@ export const apiConnector = (method, url, bodyData, headers, params) => {
     return axiosInstance({
         method: method,
         url: url,
-        data: bodyData?bodyData:null,
+        data: bodyData?bodyData:null, 
         headers: headers?headers:null,
         params: params?params:null
     })
